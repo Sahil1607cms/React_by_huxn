37a67f5ffc3421e195b1b02f9feda51de87fa70d
Sahil Srivastava <149762467+Sahil1607cms@users.noreply.github.com>
2025-01-12 19:34:06 +0530

    Update UserList.jsx

--- 01. Fundamentals/06. List of Data/exercise/solution/components/UserList.jsx
+++ 01. Fundamentals/06. List of Data/exercise/solution/components/UserList.jsx
@@ -7,7 +7,9 @@ const UserList = () => {
 
   return (
     <div>
+{/*  using .map to iterate over the array  */}
       {users.map((user) => (
+  // giving key value is necessary otherwise some error will come which u have to study later 
         <div key={user.id}>
           <h1>Name: {user.name}</h1>
           <h1>Age: {user.age}</h1>
